da80ebd8717f6046376baf8913bf0448689a2266
oligeon-dev <oligeon.dev@gmail.com>
2024-06-10 14:36:36 +0900

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -25,11 +25,23 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div>
+        <label htmlFor="num">type:number</label>
+        <input id="num" type="number" />
+      </div>
+      <div>
+        <label htmlFor="numeric">type:numer, inputMode: numeric</label>
+        <input id="numeric" type="number" inputMode="numeric" />
+      </div>
+      <div>
+        <label htmlFor="decimal">type:numer, inputMode: decimal</label>
+        <input id="decimal" type="number" inputMode="decimal" />
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
